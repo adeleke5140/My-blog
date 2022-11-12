9744b8aa4501bda4e3e1ebf21506afbbcf14cad7
Kehinde Adeleke <adelekekehinde06@gmail.com>
2022-11-12 09:19:45 +0100

    update the title of the page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,48 +3,48 @@ import * as React from "react"
 const pageStyles = {
   color: "#232129",
   padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
+  fontFamily: "-apple-system, Roboto, sans-serif, serif"
 }
 const headingStyles = {
   marginTop: 0,
   marginBottom: 64,
-  maxWidth: 320,
+  maxWidth: 320
 }
 const headingAccentStyles = {
-  color: "#663399",
+  color: "#663399"
 }
 const paragraphStyles = {
-  marginBottom: 48,
+  marginBottom: 48
 }
 const codeStyles = {
   color: "#8A6534",
   padding: 4,
   backgroundColor: "#FFF4DB",
   fontSize: "1.25rem",
-  borderRadius: 4,
+  borderRadius: 4
 }
 const listStyles = {
   marginBottom: 96,
-  paddingLeft: 0,
+  paddingLeft: 0
 }
 const listItemStyles = {
   fontWeight: 300,
   fontSize: 24,
   maxWidth: 560,
-  marginBottom: 30,
+  marginBottom: 30
 }
 
 const linkStyle = {
   color: "#8954A8",
   fontWeight: "bold",
   fontSize: 16,
-  verticalAlign: "5%",
+  verticalAlign: "5%"
 }
 
 const docLinkStyle = {
   ...linkStyle,
   listStyleType: "none",
-  marginBottom: 24,
+  marginBottom: 24
 }
 
 const descriptionStyle = {
@@ -52,13 +52,13 @@ const descriptionStyle = {
   fontSize: 14,
   marginTop: 10,
   marginBottom: 0,
-  lineHeight: 1.25,
+  lineHeight: 1.25
 }
 
 const docLink = {
   text: "Documentation",
   url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
+  color: "#8954A8"
 }
 
 const badgeStyle = {
@@ -74,7 +74,7 @@ const badgeStyle = {
   position: "relative",
   top: -2,
   marginLeft: 10,
-  lineHeight: 1,
+  lineHeight: 1
 }
 
 const links = [
@@ -83,35 +83,35 @@ const links = [
     url: "https://www.gatsbyjs.com/docs/tutorial/",
     description:
       "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
+    color: "#E95800"
   },
   {
     text: "How to Guides",
     url: "https://www.gatsbyjs.com/docs/how-to/",
     description:
       "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
+    color: "#1099A8"
   },
   {
     text: "Reference Guides",
     url: "https://www.gatsbyjs.com/docs/reference/",
     description:
       "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
+    color: "#BC027F"
   },
   {
     text: "Conceptual Guides",
     url: "https://www.gatsbyjs.com/docs/conceptual/",
     description:
       "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
+    color: "#0D96F2"
   },
   {
     text: "Plugin Library",
     url: "https://www.gatsbyjs.com/plugins",
     description:
       "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
+    color: "#8EB814"
   },
   {
     text: "Build and Host",
@@ -119,8 +119,8 @@ const links = [
     badge: true,
     description:
       "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
+    color: "#663399"
+  }
 ]
 
 const IndexPage = () => {
@@ -129,7 +129,9 @@ const IndexPage = () => {
       <h1 style={headingStyles}>
         Congratulations
         <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
+        <span style={headingAccentStyles}>
+          — you just made a Gatsby site! 🎉🎉🎉
+        </span>
       </h1>
       <p style={paragraphStyles}>
         Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
@@ -144,7 +146,7 @@ const IndexPage = () => {
             {docLink.text}
           </a>
         </li>
-        {links.map(link => (
+        {links.map((link) => (
           <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
             <span>
               <a
@@ -173,4 +175,4 @@ const IndexPage = () => {
 
 export default IndexPage
 
-export const Head = () => <title>Home Page</title>
+export const Head = () => <title>Kehinde's blog</title>
