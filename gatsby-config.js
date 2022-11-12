6bd53e10796c6335c59c43d5d52c4f1b884dd31d
Kehinde Adeleke <adelekekehinde06@gmail.com>
2022-11-12 09:08:35 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `kehinde-blog`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
