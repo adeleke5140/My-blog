4a0f2d05cd891c3ca8efb6b7e45788b69f3f0822
Kehinde Adeleke <adelekekehinde06@gmail.com>
2022-11-12 10:08:17 +0100

    add about page

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import * as React from "react"
+
+const AboutPage = () => {
+  return (
+    <main>
+      <h1>About me</h1>
+      <p>Hi there, welcome to my portfolio.</p>
+      <p>I am a proud creator of this site, which I am building with Gatsby </p>
+    </main>
+  )
+}
+
+export const Head = () => <title>About Me</title>
+
+export default AboutPage
